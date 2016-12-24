c9f13e7b07c20238f95782e9c67fcb160eb4e40d
PavanBahuguni <pavanbahuguni15@gmail.com>
2016-12-24 13:17:32 +0530

    Initial commit

--- angular2_hello_world/src/app/user-item/user-item.component.ts
+++ angular2_hello_world/src/app/user-item/user-item.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit, Input } from '@angular/core';
 export class UserItemComponent implements OnInit {
   @Input() name: String;
   constructor() {
-    this.name = 'Pavan';
   }
 
   ngOnInit() {
