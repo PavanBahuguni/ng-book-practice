e6cb0a5d2c19ec100f038098e707c73103e0fb71
Chief Editor <Chief Editor>
2017-01-13 16:30:02 +0530

    http initial commit

---
+++ http/src/app/simple-http.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-http',
+  template: `
+    <p>
+      simple-http Works!
+    </p>
+  `,
+  styles: []
+})
+export class SimpleHttpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
